bae09f71d44f28254dc9f620a267599300db123b
Ismael Ahumada <Ismael.Ahumada@solera.com>
2022-11-07 15:33:45 +0100

    Address entity class created

---
+++ Domain/Address.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CSApiRestPractice02.Domain {
+
+    [Table("address")]
+    public class Address {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("addressId")]
+        [Required]
+        public int AddressId { get;  set; }
+
+        [Required]
+        [Column("street")]
+        public string Street { get; set; }
+
+        [Required]
+        [Column("postalCode")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        [Column("province")]
+        public string province { get; set; }
+
+        [Required]
+        [Column("country")]
+        public string country { get; set; }
+
+        [Column("phone")]
+        public int phone { get; set; }
+
+    }
+
+}
